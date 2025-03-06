1b29abbddbe23bb03de5cd1a9e4087d14e1f50e6
MaxLiebsch <liebsch@dipmaxexport.com>
2025-03-06 12:57:48 +0100

    fix: move to src folder

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,33 @@
+import Footer from "@/src/components/Footer";
+import NavBar from "@/src/components/NavBar";
+import About from "@/src/components/sections/About";
+import Contact from "@/src/components/sections/Contact";
+import Hero from "@/src/components/sections/Hero";
+import Projects from "@/src/components/sections/Projects";
+import Services from "@/src/components/sections/Services";
+import Team from "@/src/components/sections/Team";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col">
+      {/* Navbar */}
+      <NavBar />
+      <main className="flex-1">
+        {/* Hero */}
+        <Hero />
+        {/* About */}
+        <About />
+        {/* Services */}
+        <Services />
+        {/* Team */}
+        <Team />
+        {/* Projects */}
+        <Projects />
+        {/* Contact Section */}
+        <Contact />
+      </main>
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+}
